5e18402c09ea9f3835e42efa0d5798887ca825db
Elkin Torres <elkin.torres@prodigious.com>
2022-09-27 18:33:38 -0500

    Initial commit

---
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,28 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyExchange {
+  @Id
+  private Long id;
+  @Column(name = "currency_from")
+  private String from;
+  @Column(name = "currency_to")
+  private String to;
+  private BigDecimal conversionMultiple;
+  private String environment;
+}
